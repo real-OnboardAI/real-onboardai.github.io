93a269b172cec66031554e6ace6d5cee38e3fcf1
Rajesh <agentic.engineer@gmail.com>
2025-06-08 01:05:45 -0500

    test change to make github pages work

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
+  base: `real-onboardai.github.io/`,
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
